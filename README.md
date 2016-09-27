a48e013065faa893fef7667f5053a0fb3daca0d6
kwan <303198069@qq.com>
2016-09-28 02:21:43 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # think-slog
 useful debug tool base on socket log
+
+### REQUIREMENT
+* nodejs
+* chrome extension(file 'chrome.crx' in resource folder)
+* additional port 1229 and port 1116 is required,please check your firewall and add it in whitelist
+
+### HOWTO
+> install socketlog-server and run it
+```
+npm install -g socketlog-server
+```
+```
+socketlog-server
+```
+> debug where you want
+```
+<?php
+
+some code...
+slog('whatever you want');
+```
+--- 
+then open chrome dev-tool,you should see debug output in console tab ^_^
---
+++ src/functions/slog.php
@@ -0,0 +1,41 @@
+<?php
+    
+    /**
+     * @description socketlog日志工具
+     * @param        $log
+     * @param string $type
+     * @param string $css
+     * @return bool|mixed
+     * @throws Exception
+     */
+    function slog($log, $type = 'log')
+    {
+        if (is_string($type)) {
+            $type = preg_replace_callback(
+                '/_([a-zA-Z])/',
+                create_function('$matches', 'return strtoupper($matches[1]);'),
+                $type
+            );
+            if (method_exists('\Cheukpang\Slog', $type) || in_array($type, Slog::$log_types)) {
+                return call_user_func(['\Cheukpang\Slog', $type], $log);
+            }
+        }
+        
+        if (is_object($type) && 'mysqli' == get_class($type)) {
+            return \Cheukpang\Slog::mysqlilog($log, $type);
+        }
+        
+        if (is_resource($type) && ('mysql link' == get_resource_type($type) || 'mysql link persistent' == get_resource_type(
+                    $type
+                ))
+        ) {
+            return \Cheukpang\Slog::mysqllog($log, $type);
+        }
+        
+        
+        if (is_object($type) && 'PDO' == get_class($type)) {
+            return \Cheukpang\Slog::pdolog($log, $type);
+        }
+        
+        throw new \Exception($type . ' is not SocketLog method');
+    }
