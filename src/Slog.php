85b26d5d21e0cab28d3d5b9e086b4141a97740d0
kwan <303198069@qq.com>
2016-12-24 22:43:33 +0800

    misc

--- src/Slog.php
+++ src/Slog.php
@@ -6,9 +6,6 @@
      */
     namespace Cheukpang;
     
-    use Exception;
-    use PDO;
-    
     /**
      * Class Slog
      * @package Cheukpang
@@ -21,262 +18,191 @@
      * @method static groupCollapsed($trace_msg)
      * @method static alert($trace_msg)
      */
-    class Slog
+    class Socket
     {
-        /**
-         * @var int $start_time 开始时间
-         */
-        public static $start_time = 0;
-        /**
-         * @var int $start_memory 开始的占用内存
-         */
-        public static $start_memory = 0;
-        /**
-         * @var int $port SocketLog 服务的http的端口号
-         */
-        public static $port = 1116;
-        /**
-         * @var array $log_types 日志类型
-         */
-        public static $log_types = [
-            'log',
-            'info',
-            'error',
-            'warn',
-            'table',
-            'group',
-            'groupCollapsed',
-            'alert',
-        ];
+        //SocketLog 服务的http的端口号
+        public $port = 1116;
         
-        /**
-         * @var self $_instance single instance
-         */
-        protected static $_instance;
-        
-        /**
-         * @var array $config could be overwrite
-         */
-        protected static $config = [
-            //接收日志的主机，需要完整的FQDN，协议部分不能少
+        protected $config = [
+            // socket服务器地址 需要完整的FQDN
             'host'                => 'http://localhost',
-            //是否显示利于优化的参数，如果允许时间，消耗内存等
-            'optimize'            => true,
+            // 是否显示加载的文件列表
             'show_included_files' => false,
-            //限制允许读取日志的client_id
+            // 日志强制记录到配置的client_id
+            'force_client_ids'    => ['kwan'],
+            // 限制允许读取日志的client_id
             'allow_client_ids'    => ['kwan'],
         ];
         
-        /**
-         * @var array $logs log pool
-         */
-        protected static $logs = [];
+        protected $css = [
+            'sql'      => 'color:#009bb4;',
+            'sql_warn' => 'color:#009bb4;font-size:14px;',
+            'error'    => 'color:#f4006b;font-size:14px;',
+            'page'     => 'color:#40e2ff;background:#171717;',
+            'big'      => 'font-size:20px;color:red;',
+        ];
+        
+        //配置强制推送且被授权的client_id
+        protected $allowForceClientIds = [];
         
         /**
-         * @param $method
-         * @param $args
-         * @return mixed
+         * 架构函数
+         * @param array $config 缓存参数
+         * @access public
          */
-        public static function __callStatic($method, $args)
+        public function __construct(array $config = [])
         {
-            if (in_array($method, static::$log_types)) {
-                array_unshift($args, $method);
-                
-                return call_user_func_array([static::getInstance(), 'record'], $args);
-            }
-            
-            return false;
-        }
-        
-        /**
-         * sql日志
-         * @param $sql
-         * @param $link
-         * @return bool
-         * @throws \Exception
-         */
-        protected static function sql($sql, $link)
-        {
-            if (is_object($link) && 'mysqli' == get_class($link)) {
-                return static::mysqliLog($sql, $link);
-            }
-            
-            if (is_resource($link) && ('mysql link' == get_resource_type($link)
-                    || 'mysql link persistent' == get_resource_type($link))
-            ) {
-                return static::mysqlLog($sql, $link);
-            }
-            
-            if (is_object($link) && 'PDO' == get_class($link)) {
-                return static::pdoLog($sql, $link);
-            }
-            
-            throw new Exception('SocketLog only support mysql/mysqli/PDO');
-        }
-        
-        /**
-         * @param     $msg
-         * @param int $trace_level
-         * @return bool|mixed
-         */
-        protected static function trace($msg, $trace_level = 1)
-        {
-            if (!static::check()) {
-                return false;
-            }
-            $traces = debug_backtrace(false);
-            $traces = array_reverse($traces);
-            $max = count($traces) - $trace_level;
-            for ($i = 0; $i < $max; $i++) {
-                $trace = $traces[$i];
-                $fun = isset($trace['class']) ? $trace['class'] . '::' . $trace['function'] : $trace['function'];
-                $file = isset($trace['file']) ? $trace['file'] : 'unknown file';
-                $line = isset($trace['line']) ? $trace['line'] : 'unknown line';
-                $trace_msg = '#' . $i . '  ' . $fun . ' called at [' . $file . ':' . $line . ']';
-                static::log($trace_msg);
+            if (!empty($config)) {
+                $this->config = array_merge($this->config, $config);
             }
         }
         
         /**
-         * mysqli日志
-         * @internal
-         * @param $sql
-         * @param $link_identifier
+         * 调试输出接口
+         * @access public
+         * @param array $log 日志信息
          * @return bool
          */
-        protected static function mysqliLog($sql, $link_identifier)
+        public function save(array $log = [])
         {
-            if (!static::check()) {
+            if (!$this->check()) {
                 return false;
             }
+            $trace = [];
+            $runtime = round(microtime(true) - $GLOBALS['_beginTime'], 10);
+            $reqs = $runtime > 0 ? number_format(1 / $runtime, 2) : '∞';
+            $time_str = ' [运行时间：' . number_format($runtime, 6) . 's][吞吐率：' . $reqs . 'req/s]';
+            $memory_use = number_format((memory_get_usage() - $GLOBALS['_startUseMems']) / 1024, 2);
+            $memory_str = ' [内存消耗：' . $memory_use . 'kb]';
+            $file_load = ' [文件加载：' . count(get_included_files()) . ']';
             
-            if (preg_match('/^SELECT /i', $sql)) {
-                //explain
-                $query = mysqli_query($link_identifier, 'EXPLAIN ' . $sql);
-                $arr = mysqli_fetch_array($query);
-                static::sqlExplain($arr, $sql);
+            if (isset($_SERVER['HTTP_HOST'])) {
+                $current_uri = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+            } else {
+                $current_uri = 'cmd:' . implode(' ', $_SERVER['argv']);
             }
-            static::sqlWhere($sql);
-            static::trace($sql, 2);
-        }
-        
-        /**
-         * mysql日志
-         * @deprecated
-         * @param $sql
-         * @param $link_identifier
-         * @return bool
-         */
-        protected static function mysqlLog($sql, $link_identifier)
-        {
-            if (!static::check()) {
-                return false;
-            }
-            if (preg_match('/^SELECT /i', $sql)) {
-                //explain
-                $query = mysql_query('EXPLAIN ' . $sql, $link_identifier);
-                $arr = mysql_fetch_array($query);
-                static::sqlExplain($arr, $sql);
-            }
-            //判断sql语句是否有where
-            static::sqlWhere($sql);
-            static::trace($sql, 2);
-        }
-        
-        /**
-         * pdo日志
-         * @internal
-         * @param $sql
-         * @param $pdo
-         * @return bool
-         */
-        protected static function pdoLog($sql, PDO $pdo)
-        {
-            if (!static::check()) {
-                return false;
-            }
-            if (preg_match('/^SELECT /i', $sql)) {
-                //explain
-                try {
-                    $pdoStatement = $pdo->query('EXPLAIN ' . $sql);
-                    if (is_object($pdoStatement) && method_exists($pdoStatement, 'fetch')) {
-                        $arr = $pdoStatement->fetch(PDO::FETCH_ASSOC);
-                        static::sqlExplain($arr, $sql);
+            // 基本信息
+            $trace[] = [
+                'type' => 'group',
+                'msg'  => $current_uri . $time_str . $memory_str . $file_load,
+                'css'  => $this->css['page'],
+            ];
+            
+            foreach ($log as $type => $val) {
+                $trace[] = [
+                    'type' => 'groupCollapsed',
+                    'msg'  => '[ ' . $type . ' ]',
+                    'css'  => isset($this->css[$type]) ? $this->css[$type] : '',
+                ];
+                foreach ($val as $msg) {
+                    if (!is_string($msg)) {
+                        $msg = var_export($msg, true);
                     }
-                } catch (Exception $e) {
-                    return false;
+                    $trace[] = [
+                        'type' => 'log',
+                        'msg'  => $msg,
+                        'css'  => '',
+                    ];
                 }
-            }
-            static::sqlWhere($sql);
-            static::trace($sql, 2);
-        }
-        
-        /**
-         * @param array $arr
-         * @param       $sql
-         */
-        private static function sqlExplain(array $arr, &$sql)
-        {
-            $arr = array_change_key_case($arr, CASE_LOWER);
-            if (false !== strpos($arr['extra'], 'Using filesort')) {
-                $sql .= ' <---################[Using filesort]';
-            }
-            if (false !== strpos($arr['extra'], 'Using temporary')) {
-                $sql .= ' <---################[Using temporary]';
-            }
-        }
-        
-        /**
-         * @param $sql
-         */
-        private static function sqlWhere(&$sql)
-        {
-            //判断sql语句是否有where
-            if (preg_match('/^UPDATE |DELETE /i', $sql) && !preg_match('/WHERE.*(=|>|<|LIKE|IN)/i', $sql)) {
-                $sql .= '<---###########[NO WHERE]';
-            }
-        }
-        
-        /**
-         * @return self
-         */
-        public static function getInstance()
-        {
-            if (static::$_instance === null) {
-                static::$_instance = new self();
+                $trace[] = [
+                    'type' => 'groupEnd',
+                    'msg'  => '',
+                    'css'  => '',
+                ];
             }
             
-            return static::$_instance;
-        }
-        
-        /**
-         * @return bool
-         */
-        protected static function check()
-        {
-            $tabId = static::getClientArg('tabid');
-            //是否记录日志的检查
-            if (!$tabId) {
-                return false;
+            if ($this->config['show_included_files']) {
+                $trace[] = [
+                    'type' => 'groupCollapsed',
+                    'msg'  => '[ file ]',
+                    'css'  => '',
+                ];
+                $trace[] = [
+                    'type' => 'log',
+                    'msg'  => implode("\n", get_included_files()),
+                    'css'  => '',
+                ];
+                $trace[] = [
+                    'type' => 'groupEnd',
+                    'msg'  => '',
+                    'css'  => '',
+                ];
             }
-            //用户认证
-            $allow_client_ids = static::getConfig('allow_client_ids');
-            if (!$allow_client_ids) {
-                return false;
+            
+            $trace[] = [
+                'type' => 'groupEnd',
+                'msg'  => '',
+                'css'  => '',
+            ];
+            
+            $tabid = $this->getClientArg('tabid');
+            if (!$client_id = $this->getClientArg('client_id')) {
+                $client_id = '';
             }
-            if (!in_array(static::getClientArg('client_id'), $allow_client_ids)) {
-                return false;
+            
+            if (!empty($this->allowForceClientIds)) {
+                //强制推送到多个client_id
+                foreach ($this->allowForceClientIds as $force_client_id) {
+                    $client_id = $force_client_id;
+                    $this->sendToClient($tabid, $client_id, $trace, $force_client_id);
+                }
+            } else {
+                $this->sendToClient($tabid, $client_id, $trace, '');
             }
             
             return true;
         }
         
         /**
-         * 获取客户端参数
-         * @param string $name
-         * @return mixed|null
+         * 发送给指定客户端
+         * @author Zjmainstay
+         * @param $tabid
+         * @param $client_id
+         * @param $logs
+         * @param $force_client_id
          */
-        protected static function getClientArg($name = '')
+        protected function sendToClient($tabid, $client_id, $logs, $force_client_id)
+        {
+            $logs = [
+                'tabid'           => $tabid,
+                'client_id'       => $client_id,
+                'logs'            => $logs,
+                'force_client_id' => $force_client_id,
+            ];
+            $msg = json_encode($logs);
+            //将client_id作为地址， server端通过地址判断将日志发布给谁
+            $address = '/' . $client_id;
+            $this->send($this->config['host'], $msg, $address);
+        }
+        
+        protected function check()
+        {
+            $tabid = $this->getClientArg('tabid');
+            //是否记录日志的检查
+            if (!$tabid && !$this->config['force_client_ids']) {
+                return false;
+            }
+            //用户认证
+            $allow_client_ids = $this->config['allow_client_ids'];
+            if (!empty($allow_client_ids)) {
+                //通过数组交集得出授权强制推送的client_id
+                $this->allowForceClientIds = array_intersect($allow_client_ids, $this->config['force_client_ids']);
+                if (!$tabid && count($this->allowForceClientIds)) {
+                    return true;
+                }
+                
+                $client_id = $this->getClientArg('client_id');
+                if (!in_array($client_id, $allow_client_ids)) {
+                    return false;
+                }
+            } else {
+                $this->allowForceClientIds = $this->config['force_client_ids'];
+            }
+            
+            return true;
+        }
+        
+        protected function getClientArg($name)
         {
             static $args = [];
             
@@ -304,71 +230,15 @@
             return null;
         }
         
-        
         /**
-         * 设置配置
-         * @param array $config
-         */
-        public static function setConfig(array $config = [])
-        {
-            $config = array_merge(static::$config, $config);
-            static::$config = $config;
-            if (static::check()) {
-                //强制初始化SocketLog实例
-                static::getInstance();
-                if ($config['optimize']) {
-                    static::$start_time = microtime(true);
-                    static::$start_memory = memory_get_usage();
-                }
-            }
-        }
-        
-        
-        /**
-         * 获得配置
-         * @param string $name
-         * @return mixed|null
-         */
-        public static function getConfig($name = '')
-        {
-            if (isset(static::$config[$name])) {
-                return static::$config[$name];
-            }
-            
-            return null;
-        }
-        
-        /**
-         * 记录日志
-         * @internal
-         * @param  string $type
-         * @param string  $msg
-         * @return bool
-         */
-        protected function record($type = '', $msg = '')
-        {
-            if (!static::check()) {
-                return false;
-            }
-            
-            static::$logs[] = [
-                'type' => $type,
-                'msg'  => $msg,
-            ];
-            
-            return true;
-        }
-        
-        /**
-         * @internal
          * @param string $host    - $host of socket server
          * @param string $message - 发送的消息
          * @param string $address - 地址
-         * @return bool
+         * @return void
          */
-        protected static function send($host = '', $message = '', $address = '/')
+        protected function send($host, $message = '', $address = '/')
         {
-            $url = $host . ':' . static::$port . $address;
+            $url = $host . ':' . $this->port . $address;
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
             curl_setopt($ch, CURLOPT_POST, true);
@@ -383,95 +253,6 @@
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
             curl_exec($ch);
             curl_close($ch);
-            
-            return true;
-        }
-        
-        /**
-         * 发送日志
-         * @internal
-         * @return bool
-         */
-        protected static function sendLog()
-        {
-            if (!static::check()) {
-                return false;
-            }
-            
-            $timeStr = '';
-            $memoryStr = '';
-            if (static::$start_time) {
-                $runtime = microtime(true) - static::$start_time;
-                $reqs = number_format(1 / $runtime, 2);
-                $timeStr = "[运行时间：{$runtime}s][吞吐率：{$reqs}req/s]";
-            }
-            if (static::$start_memory) {
-                $memory_use = number_format((memory_get_usage() - static::$start_memory) / 1024, 2);
-                $memoryStr = "[内存消耗：{$memory_use}kb]";
-            }
-            
-            if (isset($_SERVER['HTTP_HOST'])) {
-                $currentUri = $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-            } else {
-                $currentUri = 'cmd:' . implode(' ', $_SERVER['argv']);
-            }
-            array_unshift(
-                static::$logs,
-                [
-                    'type' => 'group',
-                    'msg'  => $currentUri . $timeStr . $memoryStr,
-                ]
-            );
-            
-            if (static::getConfig('show_included_files')) {
-                static::$logs[] = [
-                    'type' => 'groupCollapsed',
-                    'msg'  => 'included_files',
-                ];
-                static::$logs[] = [
-                    'type' => 'log',
-                    'msg'  => implode("\n", get_included_files()),
-                ];
-                static::$logs[] = [
-                    'type' => 'groupEnd',
-                    'msg'  => '',
-                ];
-            }
-            
-            static::$logs[] = [
-                'type' => 'groupEnd',
-                'msg'  => '',
-            ];
-            
-            $tabId = static::getClientArg('tabid');
-            if (!$clientId = static::getClientArg('client_id')) {
-                $clientId = '';
-            }
-            static::sendToClient($tabId, $clientId, static::$logs);
-        }
-        
-        /**
-         * 发送给指定客户端
-         * @param $tabId
-         * @param $clientId
-         * @param $logs
-         */
-        protected static function sendToClient($tabId, $clientId, $logs)
-        {
-            $logs = [
-                'tabid'     => $tabId,
-                'client_id' => $clientId,
-                'logs'      => $logs,
-            ];
-            $msg = json_encode($logs);
-            //将client_id作为地址， server端通过地址判断将日志发布给谁
-            $address = '/' . $clientId;
-            static::send(static::getConfig('host'), $msg, $address);
-        }
-        
-        public function __destruct()
-        {
-            static::sendLog();
         }
         
     }
--- tests/SlogTest.php
+++ tests/SlogTest.php
@@ -7,58 +7,10 @@
     class SlogTest extends TestCase
     {
         
-        protected static $_instance = null;
+        //chrome带有tabid和client_id的响应
         
-        protected static $config = [
-            //接收日志的主机，需要完整的FQDN，协议部分不能少
-            'host'                => 'http://localhost',
-            //是否显示利于优化的参数，如果允许时间，消耗内存等
-            'optimize'            => true,
-            'show_included_files' => false,
-            //限制允许读取日志的client_id
-            'allow_client_ids'    => ['kwan'],
-        ];
+        //外部请求，不带tabid和HTTP_SOCKETLOG头部的响应
         
-        public function setUp()
-        {
-            if (!extension_loaded('mysql') & !extension_loaded('mysqli') & !extension_loaded('pdo_mysql')) {
-                $this->markTestSkipped('需要的扩展没安装');
-            }
-            self::$_instance = \Cheukpang\Slog::getInstance();
-        }
-        
-        public function tearDown()
-        {
-            self::$_instance = null;
-        }
-        
-        /**
-         * @test
-         */
-        public function slogIsExists()
-        {
-            $condition = function_exists('slog');
-            $this->assertTrue($condition);
-        }
-        
-        /**
-         * @test
-         * @depends slogIsExists
-         */
-        public function getInstance()
-        {
-            $this->assertInstanceOf(\Cheukpang\Slog::class, self::$_instance);
-        }
-        
-        /**
-         * @test
-         * @depends slogIsExists
-         */
-        public function getConfig()
-        {
-            $this->assertEquals('http://localhost', self::$_instance->getConfig('host'));
-            $this->assertTrue(true, self::$_instance->getConfig('optimize'));
-            $this->assertEquals(self::$config['allow_client_ids'], self::$_instance->getConfig('allow_client_ids'));
-        }
+        //测试log发送
         
     }
